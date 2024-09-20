8f186185265ff4f5350b9a87104681c36fbf26f0
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-09-20 20:14:39 +0300

    ...

--- news-portal/src/main/java/com/example/springbootnewsportal/service/Impl/DataBaseCommentService.java
+++ news-portal/src/main/java/com/example/springbootnewsportal/service/Impl/DataBaseCommentService.java
@@ -1,4 +1,56 @@
 package com.example.springbootnewsportal.service.Impl;
 
-public class DataBaseCommentService {
+import com.example.springbootnewsportal.exception.EntityNotFoundException;
+import com.example.springbootnewsportal.model.Comment;
+import com.example.springbootnewsportal.repositories.DataBaseCommentRepository;
+import com.example.springbootnewsportal.service.CommentService;
+import com.example.springbootnewsportal.utils.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
+import java.util.List;
+
+@Service
+public class DataBaseCommentService implements CommentService
+{
+    private DataBaseCommentRepository repository;
+
+    @Override
+    public List<Comment> findAll()
+    {
+        return repository.findAll();
+    }
+
+    @Override
+    public Comment findById(Long commentId)
+    {
+        return repository.findById(commentId).orElseThrow(() ->
+                new EntityNotFoundException(MessageFormat.format("Comment with ID: {0} not found", commentId)));
+    }
+
+    @Override
+    public Comment save(Comment comment)
+    {
+        return repository.save(comment);
+    }
+
+    @Override
+    public Comment update(Comment comment)
+    {
+        Comment currentComment = findById(comment.getId());
+        BeanUtils.copyNonNullProperties(comment, currentComment);
+        return repository.save(comment);
+    }
+
+    @Override
+    public void deleteById(Long commentId)
+    {
+        repository.deleteById(commentId);
+    }
+
+    @Override
+    public void deleteByIdInd(List<Long> ids)
+    {
+        repository.deleteAllById(ids);
+    }
 }
