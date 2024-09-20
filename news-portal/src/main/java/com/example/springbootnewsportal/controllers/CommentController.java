b86afd530516ff58a60ae090c3fe856cbc27886f
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-09-20 19:13:38 +0300

    ...

--- news-portal/src/main/java/com/example/springbootnewsportal/controllers/CommentController.java
+++
@@ -1,4 +0,0 @@
-package com.example.springbootnewsportal.controllers;
-
-public class CommentController {
-}
--- news-portal/src/main/java/com/example/springbootnewsportal/repository/DBCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springbootnewsportal.repository;
-
-import com.example.springbootnewsportal.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DBCategoryRepository extends JpaRepository<Category, Long>
-{
-}
