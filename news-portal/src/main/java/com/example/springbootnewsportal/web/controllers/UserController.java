9a679a422987d1baf2d288d6006911f08568118b
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-09-23 22:01:22 +0300

    ...

--- news-portal/src/main/java/com/example/springbootnewsportal/web/controllers/UserController.java
+++ news-portal/src/main/java/com/example/springbootnewsportal/web/controllers/UserController.java
@@ -1,15 +1,15 @@
 package com.example.springbootnewsportal.web.controllers;
 
 import com.example.springbootnewsportal.mapper.UserMapper;
+import com.example.springbootnewsportal.model.User;
 import com.example.springbootnewsportal.service.UserService;
+import com.example.springbootnewsportal.web.model.user.UpsertUserRequest;
 import com.example.springbootnewsportal.web.model.user.UserListResponse;
 import com.example.springbootnewsportal.web.model.user.UserResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -28,6 +28,27 @@ public class UserController
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> findById(@PathVariable("id") Long userId)
     {
-        
+        return ResponseEntity.ok(mapper.userToResponse(userWithDataBaseService.findById(userId)));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> create(@RequestBody UpsertUserRequest request)
+    {
+        User newUser = userWithDataBaseService.save(mapper.requestToUser(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(mapper.userToResponse(newUser));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> update(@PathVariable("id") Long userId, @RequestBody UpsertUserRequest request)
+    {
+        User updatedUser = userWithDataBaseService.update(mapper.requestToUser(userId, request));
+        return ResponseEntity.ok(mapper.userToResponse(updatedUser));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long userId)
+    {
+        userWithDataBaseService.delete(userId);
+        return ResponseEntity.noContent().build();
     }
 }
