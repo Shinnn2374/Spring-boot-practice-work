192f0627094807931623ee0079aa06dea3a9eb84
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-09-25 20:54:04 +0300

    ....

--- news-portal/src/main/java/com/example/springbootnewsportal/service/impl/DBCommentService.java
+++ news-portal/src/main/java/com/example/springbootnewsportal/service/impl/DBCommentService.java
@@ -1,4 +1,54 @@
 package com.example.springbootnewsportal.service.impl;
 
-public class DBCommentService {
+import com.example.springbootnewsportal.exception.EntityNotFoundException;
+import com.example.springbootnewsportal.model.Comment;
+import com.example.springbootnewsportal.model.User;
+import com.example.springbootnewsportal.repository.DBCommentRepository;
+import com.example.springbootnewsportal.service.CommentService;
+import com.example.springbootnewsportal.utils.BeanUtils;
+
+import java.text.MessageFormat;
+import java.util.List;
+
+public class DBCommentService implements CommentService
+{
+    private DBCommentRepository repository;
+    private DBUserService service;
+
+    @Override
+    public List<Comment> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Comment findById(Long commentId) {
+        return repository.findById(commentId).orElseThrow(() ->
+                new EntityNotFoundException(MessageFormat.format("Comment with ID: {0} not found", commentId)));
+    }
+
+    @Override
+    public Comment save(Comment comment) {
+        User saveUser = service.findById(comment.getId());
+        comment.setAuthor(saveUser);
+        return repository.save(comment);
+    }
+
+    @Override
+    public Comment update(Comment comment) {
+        User updateUser = service.findById(comment.getAuthor().getId());
+        Comment existComment = findById(comment.getId());
+        existComment.setAuthor(updateUser);
+        BeanUtils.copyNonNullProperties(comment, existComment);
+        return repository.save(comment);
+    }
+
+    @Override
+    public void deleteById(Long commentId) {
+        repository.deleteById(commentId);
+    }
+
+    @Override
+    public void deleteByIdIn(List<Long> commentIds) {
+        repository.deleteAllById(commentIds);
+    }
 }
--- news-portal/src/main/java/com/example/springbootnewsportal/service/impl/DBUserService.java
+++ news-portal/src/main/java/com/example/springbootnewsportal/service/impl/DBUserService.java
@@ -1,17 +1,22 @@
 package com.example.springbootnewsportal.service.impl;
 
+import com.example.springbootnewsportal.exception.EntityNotFoundException;
 import com.example.springbootnewsportal.model.User;
 import com.example.springbootnewsportal.repository.DBUserRepository;
 import com.example.springbootnewsportal.service.UserService;
+import com.example.springbootnewsportal.utils.BeanUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DBUserService implements UserService
 {
+    @Autowired
     private DBUserRepository repository;
 
     @Override
@@ -21,21 +26,24 @@ public class DBUserService implements UserService
 
     @Override
     public User findById(Long userId) {
-        return repository.findById(userId).orElse(null);
+        return repository.findById(userId).orElseThrow(() ->
+                new EntityNotFoundException(MessageFormat.format("User with ID: {0} not found", userId)));
     }
 
     @Override
     public User save(User user) {
-        return null;
+        return repository.save(user);
     }
 
     @Override
     public User update(User user) {
-        return null;
+        User updateUser = new User();
+        BeanUtils.copyNonNullProperties(user, updateUser);
+        return repository.save(updateUser);
     }
 
     @Override
     public void deleteById(Long userId) {
-
+        repository.deleteById(userId);
     }
 }
