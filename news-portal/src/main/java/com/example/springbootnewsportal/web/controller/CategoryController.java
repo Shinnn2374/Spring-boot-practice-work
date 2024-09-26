3b13b39770c19a87e5f0b1bd1278fef451135874
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-09-26 11:34:44 +0300

    ....

--- news-portal/src/main/java/com/example/springbootnewsportal/web/controller/CategoryController.java
+++ news-portal/src/main/java/com/example/springbootnewsportal/web/controller/CategoryController.java
@@ -1,9 +1,15 @@
 package com.example.springbootnewsportal.web.controller;
 
+import com.example.springbootnewsportal.mapper.CategoryMapper;
+import com.example.springbootnewsportal.model.Category;
 import com.example.springbootnewsportal.service.CategoryService;
+import com.example.springbootnewsportal.web.model.category.CategoryListResponse;
+import com.example.springbootnewsportal.web.model.category.CategoryResponse;
+import com.example.springbootnewsportal.web.model.category.UpsertCategoryRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/category")
@@ -11,4 +17,38 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController
 {
     private CategoryService service;
+    private CategoryMapper mapper;
+
+    @GetMapping
+    public ResponseEntity<CategoryListResponse> findAll()
+    {
+        return ResponseEntity.ok(mapper.categoryListToCategoryListResponse(service.findAll()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> findById(@PathVariable("id") Long categoryId)
+    {
+        return ResponseEntity.ok(mapper.categoryToResponse(service.findById(categoryId)));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> create(@RequestBody UpsertCategoryRequest request)
+    {
+        Category newCategory = service.save(mapper.requestToCategory(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(mapper.categoryToResponse(newCategory));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponse> update(@PathVariable("id") Long categoryId, UpsertCategoryRequest request)
+    {
+        Category updatedCategory = service.update(mapper.requestToCategory(request));
+        return ResponseEntity.ok(mapper.categoryToResponse(updatedCategory));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long categoryId)
+    {
+        service.deleteById(categoryId);
+        return ResponseEntity.noContent().build();
+    }
 }
--- news-portal/src/main/java/com/example/springbootnewsportal/web/controller/NewsController.java
+++ news-portal/src/main/java/com/example/springbootnewsportal/web/controller/NewsController.java
@@ -1,9 +1,15 @@
 package com.example.springbootnewsportal.web.controller;
 
+import com.example.springbootnewsportal.mapper.NewsMapper;
+import com.example.springbootnewsportal.model.News;
 import com.example.springbootnewsportal.service.NewsService;
+import com.example.springbootnewsportal.web.model.news.NewsListResponse;
+import com.example.springbootnewsportal.web.model.news.NewsResponse;
+import com.example.springbootnewsportal.web.model.news.UpsertNewsRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/news")
@@ -11,4 +17,38 @@ import org.springframework.web.bind.annotation.RestController;
 public class NewsController
 {
     private NewsService service;
+    private NewsMapper mapper;
+
+    @GetMapping
+    public ResponseEntity<NewsListResponse> findAll()
+    {
+        return ResponseEntity.ok(mapper.newsListToNewsListResponse(service.findAll()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<NewsResponse> findById(@PathVariable Long id)
+    {
+        return ResponseEntity.ok(mapper.newsToResponse(service.findById(id)));
+    }
+
+    @PostMapping
+    public ResponseEntity<NewsResponse> create(@RequestBody UpsertNewsRequest request)
+    {
+        News newNews = service.save(mapper.requestToNews(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(mapper.newsToResponse(newNews));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<NewsResponse> update(@PathVariable("id") Long newsId, @RequestBody UpsertNewsRequest request)
+    {
+        News updatedNews = service.update(mapper.requestToNews(newsId, request));
+        return ResponseEntity.ok(mapper.newsToResponse(updatedNews));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long newsId)
+    {
+        service.deleteById(newsId);
+        return ResponseEntity.noContent().build();
+    }
 }
