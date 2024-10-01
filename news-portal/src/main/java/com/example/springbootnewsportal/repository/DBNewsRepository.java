cb99b0b54a7ff7e7c205813b34be9380159cad46
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-10-01 19:44:55 +0300

    ...

--- news-portal/src/main/java/com/example/springbootnewsportal/repository/DBNewsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springbootnewsportal.repository;
-
-import com.example.springbootnewsportal.model.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DBNewsRepository extends JpaRepository<News,Long>
-{
-}
--- news-portal/src/main/java/com/example/springbootnewsportal/repository/DBUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springbootnewsportal.repository;
-
-import com.example.springbootnewsportal.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface  DBUserRepository extends JpaRepository<User, Long>
-{
-}
