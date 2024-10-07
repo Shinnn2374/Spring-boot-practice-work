d2823e7e131a982e96b01d5f54e6cac3c2a70608
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-10-07 23:38:58 +0300

    ;..

--- news-portal/src/main/java/com/example/springbootnewsportal/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootnewsportal.repository;
-
-import com.example.springbootnewsportal.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- news-portal/src/main/java/com/example/springbootnewsportal/repository/NewsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootnewsportal.repository;
-
-import com.example.springbootnewsportal.model.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsRepository extends JpaRepository<News, Long> {
-}
