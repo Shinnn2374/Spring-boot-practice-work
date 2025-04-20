5f2adacef117838f65f4c68dbcb159ce709a14b3
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-04-20 18:13:04 +0300

    ...

--- simple-task-tracker/news-portal/src/main/java/com/example/springbootnewsportal/model/User.java
+++ simple-task-tracker/news-portal/src/main/java/com/example/springbootnewsportal/model/User.java
@@ -1,11 +1,14 @@
 package com.example.springbootnewsportal.model;
 
+import com.example.springbootnewsportal.utils.RoleType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -22,8 +25,15 @@ public class User
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    @Column(nullable = false)
+    private String password;
     @OneToMany
     private List<Comment> comments = new ArrayList<>();
     @OneToMany
     private List<Posts> posts = new ArrayList<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<RoleType> roles = new HashSet<>();
 }
---
+++ simple-task-tracker/news-portal/src/main/java/com/example/springbootnewsportal/utils/RoleType.java
@@ -0,0 +1,7 @@
+package com.example.springbootnewsportal.utils;
+
+public enum RoleType {
+    ROLE_ADMIN,
+    ROLE_USER,
+    ROLE_MODERATOR
+}
